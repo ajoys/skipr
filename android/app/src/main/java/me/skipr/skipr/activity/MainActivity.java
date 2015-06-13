0aa5f306834f2635cca3fa8f99016e0737e4d813
Navjot Panesar <NavjotSPanesar@gmail.com>
2015-06-13 16:32:50 -0400

    Added MainFragment, organized package names

---
+++ android/app/src/main/java/me/skipr/skipr/activity/MainActivity.java
@@ -0,0 +1,51 @@
+package me.skipr.skipr.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import me.skipr.skipr.R;
+import me.skipr.skipr.fragment.MainFragment;
+
+
+public class MainActivity extends BaseActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            String groupId = getIntent().getExtras().getString("group_id", "");
+            MainFragment mainFragment = MainFragment.newInstance(groupId) ;
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, mainFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
