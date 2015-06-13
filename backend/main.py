63ec7491e8188d6736d3748895624b831f45474b
Aaron Lobo <lobo.aaron1@gmail.com>
2015-06-13 13:46:07 -0400

    Setup Flask for bluemix

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 import requests
 import secrets
 
@@ -9,5 +10,6 @@ app = Flask(__name__)
 def hello():
     return 'Hello World! Skipr Test :D'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
