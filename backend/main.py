f152ab5b965e3081abf14a74ddb9d38ada2b1132
Aaron Lobo <lobo.aaron1@gmail.com>
2015-06-13 16:14:21 -0400

    Add static webpage support

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 import os
 import requests
 import secrets
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/hello')
 def hello():
-    return 'Hello World! Skipr Test :D'
+    return 'Hello World! (Skipr Test)'
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port), debug=True)
