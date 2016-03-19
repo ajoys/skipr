4d5c1d4361125392007300330317cfddc7dfc75f
Navjot <NavjotSPanesar@gmail.com>
2016-03-19 12:57:08 -0700

    Forgot the schema file (whoops...)

---
+++ backend-node/routes/schema.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TrackSchema = new Schema({
+    name: String,
+    album: String,
+    'image': String,
+    'artist': String,
+    'artist_id': String,
+    'popularity': Number,
+    'id': String,
+    'votes': 0,
+    'voters': [String]
+});
+
+var RoomSchema = new Schema({
+        createdAt: { type: Date, default: Date.now },
+        name: String,
+        owner: String,
+        token: String,
+        users: [Number],
+        tracks: [TrackSchema]
+    }
+);
+
+exports.Track = mongoose.model('Track', TrackSchema);
+exports.Room = mongoose.model('Room', RoomSchema);
